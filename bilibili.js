a4eeb581d1a4eeca7ef1dadb38c60c484050ce2e
misdake <misdake@gmail.com>
2019-08-29 14:47:00 +0800

    check todo list periodically. download unfinished video on todo list and check item.

---
+++ bilibili.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const spawn = require('child_process').spawn;
+
+/**
+ * download bilibili video by the video code (like av12345)
+ * @param videoCode
+ * @returns {boolean} downloaded succeeded or not
+ */
+function download(videoCode, callback) {
+    const downloadFolder = 'repo\\' + videoCode;
+
+    if (!fs.existsSync(downloadFolder)) {
+        fs.mkdirSync(downloadFolder, {recursive: true});
+    }
+
+    const ls = spawn('downloader/annie', ['-c', 'downloader/cookies.txt', '-o', './repo/' + videoCode, '-C', videoCode]);
+
+    ls.stdout.on('data', (data) => {
+        // console.log(`stdout: ${data}`);
+    });
+
+    ls.stderr.on('data', (data) => {
+        // console.log(`stderr: ${data}`);
+    });
+
+    ls.on('close', (code) => {
+        //TODO safe out/err output to file if error occured
+        console.log(`child process exited with code ${code}`);
+        callback(code);
+    });
+}
+
+module.exports = {
+    download: download
+};
--- main.js
+++ main.js
@@ -1,46 +1,58 @@
 const https = require('https');
-const fs = require('fs');
-const spawn = require('child_process').spawn;
+const {download} = require("./bilibili");
 
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
-https.get('https://rgbuv.xyz/java/todos/list/bilibili-repo', (resp) => {
-    let data = '';
+let check = null;
 
-    // A chunk of data has been recieved.
-    resp.on('data', (chunk) => {
-        data += chunk;
-    });
-
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-        let list = JSON.parse(data);
-        for (let item of list.list) {
-            console.log(item);
-        }
-    });
-
-}).on("error", (err) => {
-    console.log("Error: " + err.message);
-});
-
-const videoCode = process.argv[2] || 'av19536380';
-const downloadFolder = 'repo\\' + videoCode;
-
-if (!fs.existsSync(downloadFolder)) {
-    fs.mkdirSync(downloadFolder);
+function next() {
+    console.log("check");
+    setTimeout(check, 2000);
 }
 
-const ls = spawn('downloader/annie', ['-c', 'downloader/cookies.txt', '-o', './repo/' + videoCode, '-C', videoCode]);
+//TODO rewrite with promise
+check = function () {
+    https.get('https://rgbuv.xyz/java/todos/list/bilibili-repo', (resp) => {
+        let data = '';
 
-ls.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-});
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
 
-ls.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
-});
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            let todo = null;
+            let list = JSON.parse(data);
+            for (let item of list.list) {
+                if (item.name.match(/^av[0-9]+$/) && !item.finished) {
+                    todo = item;
+                    break;
+                }
+            }
 
-ls.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-});
+            if (todo) {
+                let item = todo;
+                console.log("working on:", item.name);
+                download(item.name, function (returncode) {
+                    if (returncode === 0) {
+                        console.log("finished:", item.name);
+                        https.get(`https://rgbuv.xyz/java/todos/item/bilibili-repo/${item.id}/check`);
+                    } else {
+                        console.log("failed:", item.name);
+                        https.get(`https://rgbuv.xyz/java/todos/item/bilibili-repo/${item.id}/check`);
+                        https.get(`https://rgbuv.xyz/java/todos/item/bilibili-repo/fail_${item.name}/add`);
+                    }
+                    next();
+                });
+            } else {
+                next();
+            }
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+};
+
+next();
