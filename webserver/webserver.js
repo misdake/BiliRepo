80ce8fea7629046000a2c927968314247ed4096a
misdake <misdake@gmail.com>
2019-09-30 15:49:27 +0800

    add storage. read current repo.

--- webserver/webserver.js
+++ webserver/webserver.js
@@ -1,5 +1,11 @@
+const Storage = require('./storage');
 const express = require('express');
 const cors = require('cors');
+
+console.log("loading videos");
+let storage = new Storage();
+storage.loadAllVideos();
+console.log("complete");
 
 const app = express();
 
@@ -8,21 +14,20 @@ app.use(cors());
 app.use('/', express.static('page')); //provide web pages
 app.use('/repo', express.static('repo')); //provide media files (video, danmaku, videoinfo)
 
-app.get('/api/video/all', function (req, res) {
-
-});
-app.get('/api/video/tag/:tag', function (req, res) {
-
-});
-app.get('/api/video/new', function (req, res) {
+app.get('/api/video/random', function (req, res) {
 
 });
 app.get('/downloader/finished', function (req, res) { //from downloader, read video info
 
 });
 
-app.get('/video/:aid', function (req, res) {
-    res.send(`video: aid = ${req.params.aid}`);
+app.get('/api/video/aid/:aid', function (req, res) {
+    let video = storage.video_aid(parseInt(req.params.aid));
+    if (video) {
+        res.send(video);
+    } else {
+        res.status(404).end();
+    }
 });
 
 app.listen(8081);
